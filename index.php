6ac18a7c590de3c1ac847aea831709627883b375
thorongil3 <thorongil3@gmail.com>
2013-08-02 14:40:59 -0700

    Create index.php
    First test of github

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>  
+	<link type="text/css" href="style/main.css" rel="stylesheet" media="all" />
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+	</script><title>How to Build a Basic CSS Layout</title>
+	<meta name="Description" content="How to Build a Basic CSS Layout" />
+	<meta name="Keywords" content="css layout" />
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header">
+		</div>
+		<h1>Basic CSS Layout</h1>
+		<div id="nav">
+		</div>
+		<h2>Navigation</h2>
+		<div id="main">
+		</div>
+		<div id="footer">
+		</div>
+	</div>
+</body>
+</html>
