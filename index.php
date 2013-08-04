f1befa6faf5747a6eb2f6530b484087e8a4e61ff
thorongil3 <thorongil3@gmail.com>
2013-08-03 21:17:17 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,24 @@
 <link type="text/css" href="style/main.css" rel="stylesheet" media="screen" />
 <title>No.1 :: CSS Template :: XTML 1.0 Transitional</title>
 
+<!-- There is no log in screen at this point. Later we may create a login screen
+in the index.php file and migrate all of this code to another page
+-->
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
 <div id="container">
 	<div id="banner" >
-		<h1>test banner</h1>
+		<h1>Personal History Archive</h1>
 	</div>
 	<div id="left">
+		<a href="">Manage Photo Album</a></br></br>
+		<a href="">Track Exercise</a></br></br>
+		<a href="">Track Goals/Life Events</a></br></br>
+		<a href="">Journal</a></br></br>
+		<a href="">Edit User Interface</a></br></br>
+		<a href="">Infographic Options</a></br></br>
 	</div>
 	<div id="right">
 	</div>	
