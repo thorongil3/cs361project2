828dc2574eeea8f30d31d1d2a19c5eda2fab7394
thorongil3 <thorongil3@gmail.com>
2013-08-03 20:50:23 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,24 @@
-<html>
-<head>	
-	<link type="text/css" href="style/main.css" rel="stylesheet" media="all" />
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
-	</script><title>How to Build a Basic CSS Layout</title>
-	<meta name="Description" content="How to Build a Basic CSS Layout" />
-	<meta name="Keywords" content="css layout" />
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link type="text/css" href="style/main.css" rel="stylesheet" media="screen" />
+<title>No.1 :: CSS Template :: XTML 1.0 Transitional</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
-	<div id="wrapper">
-		<div id="header">
-			<h1>Basic CSS Layout</h1>
-		</div>
-		<div id="nav">
-			<h2>Navigation</h2>
-		</div>
-		<div id="main">
-			
-		</div>
-		<div id="footer">
-		</div>
+<div id="container">
+	<div id="banner" >
+		<h1>test banner</h1>
 	</div>
+	<div id="left">
+	</div>
+	<div id="right">
+	</div>	
+	<div id="content">
+	</div>
+ 	<div id="footer">
+	</div>
+</div>
 </body>
 </html>
